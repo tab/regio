82b681d43c34f75fcfddbb62f05b4b0e384fe3af
Andrey Ashgaliyev <a.ashgaliev@gmail.com>
2022-12-19 17:16:44 +0500

    Add routing feature

---
+++ spec/lib/regio/routing_spec.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+RSpec.describe Regio::Routing do
+  include RegioHelpers
+
+  it 'has a base_uri' do
+    expect(described_class.base_uri).not_to be_nil
+  end
+
+  describe '#results' do
+    subject(:results) { described_class.new(options).results }
+
+    shared_examples 'raise unprocessable error' do
+      it do
+        expect { results }.to raise_error(Regio::Unprocessable, 'No API key found in request')
+      end
+    end
+
+    shared_examples 'respond with ok response' do
+      it { expect(results).to eq(response) }
+    end
+
+    context 'when options is empty' do
+      let(:options) do
+        {}
+      end
+
+      before do
+        regio_stub_request(:get, '/routing').to_return(regio_response('routing/error.json'))
+      end
+
+      include_examples 'raise unprocessable error'
+    end
+
+    context 'when api key is not provided' do
+      let(:options) do
+        {
+          apikey: 'not-a-key'
+        }
+      end
+
+      before do
+        regio_stub_request(:get, '/routing').to_return(regio_response('routing/error.json'))
+      end
+
+      include_examples 'raise unprocessable error'
+    end
+
+    context 'when invalid coordinates provided' do
+      let(:options) do
+        {
+          coordinates: [[224.649138022268, 59.14048887149], [25.853136227622, 59.00678681919]]
+        }
+      end
+
+      before do
+        regio_stub_request(:get, '/routing').to_return(regio_response('routing/error.json'))
+      end
+
+      include_examples 'raise unprocessable error'
+    end
+
+    context 'when query is valid' do
+      before do
+        regio_stub_request(:get, '/routing').to_return(regio_response('routing/ok.json'))
+      end
+
+      let(:response) do
+        JSON.parse(File.read('./spec/fixtures/routing/ok.json'), symbolize_names: true)
+      end
+
+      let(:options) do
+        {
+          coordinates: [[24.649138022268, 59.14048887149], [25.853136227622, 59.00678681919]]
+        }
+      end
+
+      include_examples 'respond with ok response'
+    end
+  end
+end
